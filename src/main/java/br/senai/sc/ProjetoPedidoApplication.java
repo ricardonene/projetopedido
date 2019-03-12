21f79f416dfec09b7d9d22d2cae5282bc1a3a996
Viviany Pfleger <pflegerviviany@gmail.com>
2019-03-11 22:07:12 -0300

    CategoriaRepository

--- src/main/java/br/senai/sc/ProjetoPedidoApplication.java
+++ src/main/java/br/senai/sc/ProjetoPedidoApplication.java
@@ -1,13 +1,34 @@
 package br.senai.sc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.senai.sc.domain.Categoria;
+import br.senai.sc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class ProjetoPedidoApplication {
+public class ProjetoPedidoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoPedidoApplication.class, args);
 	}
 
+	@Autowired
+	private CategoriaRepository repo;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		repo.save(cat1);
+		repo.save(cat2);
+	}
+
+	
+	
+	
+	
 }
--- src/main/java/br/senai/sc/domain/Categoria.java
+++ src/main/java/br/senai/sc/domain/Categoria.java
@@ -1,8 +1,17 @@
 package br.senai.sc.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+
 	private String nome;
 	
 	public Categoria() {
