e5ef613632b2c554c7c3349c59fca1e741e13319
Ricardo Nenê <ricardonene@gmail.com>
2019-03-12 21:49:48 -0300

    Criacao Produto e relacionamento ManyToMany Categoria

--- src/main/java/br/senai/sc/ProjetoPedidoApplication.java
+++ src/main/java/br/senai/sc/ProjetoPedidoApplication.java
@@ -1,31 +1,65 @@
 package br.senai.sc;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.senai.sc.domain.Categoria;
+import br.senai.sc.domain.Produto;
 import br.senai.sc.repositories.CategoriaRepository;
+import br.senai.sc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class ProjetoPedidoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoPedidoApplication.class, args);
-	}
-
-	@Autowired
-	private CategoriaRepository repo;
 	
+	}
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
-		repo.save(cat1);
-		repo.save(cat2);
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		
+		p1.getCategorias().add(cat1);
+		cat1.getProdutos().add(p1);
+		
+		categoriaRepository.save(cat1);
+		categoriaRepository.save(cat2);
+		
+		produtoRepository.save(p1);
+		produtoRepository.save(p2);
+		
 	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 	
 	
